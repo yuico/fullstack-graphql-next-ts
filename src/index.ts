a5cd0baf1d74aa3a454cef3e99705f716efb0175
yui kim <nokdoo00@gmail.com>
2021-07-26 00:50:01 -0400

    feat: add post entities and basic set for MikroORM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Test2');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './contants';
+import { Post } from './entities/Post';
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: 'lireddit',
+    type: 'postgresql',
+    debug: !__prod__,
+  });
+
+  const post = orm.em.create(Post, { title: 'my first post' });
+  //const post = new Post('my first post')
+  await orm.em.persistAndFlush(post);
+  console.log('-----sql 2------');
+
+  await orm.em.nativeInsert(Post, { title: 'my first post 2' });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
